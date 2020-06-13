7062039e56054e20e0fd3ae3ad318acb8af310a6
Amro Alnajar <amro.alnajar92@gmail.com>
2020-06-13 18:04:10 +0200

    Add test for resizing images and a test image

--- Tests/ImageTests.cs
+++ Tests/ImageTests.cs
@@ -1,10 +1,28 @@
 using NUnit.Framework;
 using EditMe;
+using System;
+using System.IO;
 
 namespace Tests
 {
     public class ImageTests
     {
+        static string path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\testImage.jpg"));
+
+
+         [Test]
+        public void Should_Resize_Image_When_Called()
+        {
+            var width = 233;
+            var height = 300;
+
+            var newImage = ImageEditor.ResizeImage(path, width,height);        
+
+            Assert.AreEqual(newImage.Width, width);
+            Assert.AreEqual(newImage.Height, height);
+
+        }
+
        
     }
 }
